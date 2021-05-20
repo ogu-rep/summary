08862213a97c70b2111404b24364ade92588fda3
sogu <66258304+ogu-rep@users.noreply.github.com>
2021-05-20 17:29:53 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ request.onload = function () {
   ask_elem.innerHTML = data.ask.toLocaleString() + "円";
   bid_elem.innerHTML = data.bid.toLocaleString() + "円";
   mid_elem.innerHTML = data.mid.toLocaleString() + "円";
-  date_elem.innerHTML = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
+  date_elem.innerHTML = date.toLocaleString();
   // console.log(data);
 };
+
+function showClock() {
+  let date_elem = new Date();
+  let year = date_elem.getFullYear();
+  let month = date_elem.getMonth();
+  let date = date_elem.getDate();
+  let day = date_elem.getDay();
+  let dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][day] ;	
+  let hour = date_elem.getHours();
+  let minutes  = date_elem.getMinutes();
+  let second  = date_elem.getSeconds();
+  let now_time = year + "年" + (month + 1) + "月" + date + "日" + "(" + dayOfWeekStr + ")" + " " + hour + "時" + minutes + "分" + second + "秒";
+  document.getElementById("real_time").innerHTML = now_time;
+}
+setInterval('showClock()',1000);
 request.send();
